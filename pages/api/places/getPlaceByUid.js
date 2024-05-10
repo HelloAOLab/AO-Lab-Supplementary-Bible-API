7fff28161e25f1538eb00c9dc841b2247cd4665d
Bored-Wizard <sujanbarman12345q@gmail.com>
2024-05-11 05:13:22 +0530

    first commit

---
+++ pages/api/places/getPlaceByUid.js
@@ -0,0 +1,89 @@
+import { removeParentDuplicate } from "@/lib/utils";
+import { connectToMongoDB, db } from "../../../lib/mongodb/mongodb";
+export default async function handler(req, res) {
+    try{
+        if(!db){
+            await connectToMongoDB();
+        }
+
+        const {uid} = req.body;
+
+        const collection = db.collection('biblegraphs');
+
+        const placeGraph = await collection.aggregate([
+            {
+                $match: {
+                    "uid": uid
+                }
+            },
+            {
+                $graphLookup: {
+                    from: "biblegraphs",
+                    startWith: uid,
+                    connectFromField: "verses",
+                    connectToField: "uid",
+                    as: "verses"
+                }
+            },
+            {
+                $graphLookup: {
+                    from: "biblegraphs",
+                    startWith: uid,
+                    connectFromField: "eastons",
+                    connectToField: "uid",
+                    as: "eastons"
+                }
+            },
+            {
+                $graphLookup: {
+                    from: "biblegraphs",
+                    startWith: uid,
+                    connectFromField: "peopleDied",
+                    connectToField: "uid",
+                    as: "peopleDied"
+                }
+            },
+            {
+                $graphLookup: {
+                    from: "biblegraphs",
+                    startWith: uid,
+                    connectFromField: "eventsHere",
+                    connectToField: "uid",
+                    as: "eventsHere"
+                }
+            },
+            {
+                $graphLookup: {
+                    from: "biblegraphs",
+                    startWith: uid,
+                    connectFromField: "booksWritten",
+                    connectToField: "uid",
+                    as: "booksWritten"
+                }
+            },
+            {
+                $graphLookup: {
+                    from: "biblegraphs",
+                    startWith: uid,
+                    connectFromField: "peopleBorn",
+                    connectToField: "uid",
+                    as: "peopleBorn"
+                }
+            }
+        ]).toArray();
+
+        let place = removeParentDuplicate(placeGraph[0], ["eastons", "peopleDied", "eventsHere", "booksWritten", "peopleBorn", "verses"], uid)
+        res.send({
+            data: {
+                place
+            },
+            status: 200
+        })
+    }catch(err){
+        console.log(err)
+        res.status(500).send({
+            data: err,
+            status: 500
+        })
+    }
+}
---
+++ pages/api/places/getPlaces.js
@@ -0,0 +1,25 @@
+import { connectToMongoDB, db } from "../../../lib/mongodb/mongodb";
+export default async function handler(req, res) {
+    try{
+        if(!db){
+            await connectToMongoDB();
+        }
+
+        const collection = db.collection('biblegraphs');
+
+        const {page, count} = req.body;
+
+        const places = await collection.find({type: "place"}).sort({placeID: 1}).skip((page - 1) * count).limit(count).toArray()
+
+        res.send({
+            data: places,
+            status: 200
+        })
+    }catch(err){
+        console.log(err)
+        res.status(500).send({
+            data: err,
+            status: 500
+        })
+    }
+}
