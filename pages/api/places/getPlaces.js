035c166485d58b481de43b7b95a7ea14f92fcebd
Bored-Wizard <sujanbarman12345q@gmail.com>
2024-05-11 05:29:25 +0530

    added variable conversion

--- pages/api/places/getPlaces.js
+++ pages/api/places/getPlaces.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
         const {page, count} = req.query;
 
-        const places = await collection.find({type: "place"}).sort({placeID: 1}).skip((page - 1) * count).limit(count).toArray()
+        const places = await collection.find({type: "place"}).sort({placeID: 1}).skip((parseInt(page) - 1) * parseInt(count)).limit(parseInt(count)).toArray()
 
         res.send({
             data: places,
