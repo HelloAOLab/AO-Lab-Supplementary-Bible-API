5f36490809e37093f494a5638bde4847910e0e2a
Bored-Wizard <sujanbarman12345q@gmail.com>
2024-06-01 08:33:18 +0530

    test

---
+++ pages/api/sim/editItem.js
@@ -0,0 +1,52 @@
+import { removeParentDuplicate } from "@/lib/utils";
+import { connectToMongoDB, db } from "../../../lib/mongodb/mongodb";
+export default async function handler(req, res) {
+    try{
+        res.setHeader('Access-Control-Allow-Origin', 'https://ao.bot');
+        if(!db){
+            await connectToMongoDB();
+        }
+
+        const {uid, tagsData} = req.query;
+
+        const collection = db.collection('biblegraphs');
+
+        // find a item with a unique uid
+        const sim = await collection.findOne({uid: uid});
+
+        let newSim = {
+            ...sim,
+            sims: [...JSON.parse(tagsData)]
+        }
+
+        await collection.updateOne({uid: uid}, {$set: newSim});
+
+        res.send({
+            data: newSim,
+            status: 200
+        });
+
+        // const collection = db.collection('biblegraphs');
+
+        // const simGraph = await collection.aggregate([
+        //     {
+        //         $match: {
+        //             "uid": uid
+        //         }
+        //     }
+        // ]).toArray();
+
+        // let sim = removeParentDuplicate(simGraph[0], [], uid)
+        // res.send({
+        //     data: {
+        //         ...sim
+        //     },
+        //     status: 200
+        // })
+    }catch(err){
+        res.status(500).send({
+            data: err,
+            status: 500
+        })
+    }
+}
