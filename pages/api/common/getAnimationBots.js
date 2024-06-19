bcfa3e7fb33ef6f3acac1ba57dcc2e9abb321e0f
Bored-Wizard <sujanbarman12345q@gmail.com>
2024-06-20 04:57:29 +0530

    added nedb databases and testing it

--- pages/api/common/getAnimationBots.js
+++ pages/api/common/getAnimationBots.js
@@ -5,15 +5,13 @@ export default async function handler(req, res) {
         res.setHeader('Access-Control-Allow-Origin', 'https://ao.bot');
         if (req.method === 'POST') {
 
-            let result = db.find({_id: req.body.id}, (err, newDox) => {
+            db.find({_id: req.body.id}, (err, newDox) => {
                 if (err) throw err
-                return newDox
+                res.send({
+                    data: newDox,
+                    status: 200
+                })
             });
-
-            res.send({
-                data: result,
-                status: 200
-            })
         } else {
             res.status(405).end(); // Method Not Allowed
         }
