a63fbb686a379ece1f36fbc9b6215c812b42bd71
Bored-Wizard <sujanbarman12345q@gmail.com>
2024-06-28 08:46:48 +0530

    test

--- pages/api/playlist/postPlaylist.js
+++ pages/api/playlist/postPlaylist.js
@@ -1,40 +1,36 @@
 import { connectToMongoDB, db } from "../../../lib/mongodb/mongodb";
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        try{
-            res.setHeader('Access-Control-Allow-Origin', 'https://ao.bot/');
-            res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
-            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    try{
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-            const {query} = req.body;
-    
-            if(!db){
-                await connectToMongoDB();
-            }
-    
-            const collection = db.collection('playlist');
-            const uid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-            await collection.insertOne({
-                query,
-                type: "playlist",
-                uid,
-            });
-    
-            res.send({
-                data: {
-                    uid
-                },
-                status: 200
-            })
-        }catch(err){
-            console.log(err)
-            res.status(500).send({
-                data: err,
-                status: 500
-            })
+        const {query} = req.body;
+
+        if(!db){
+            await connectToMongoDB();
         }
-    } else {
-        res.status(405).json({ message: 'Method Not Allowed' });
+
+        const collection = db.collection('playlist');
+        const uid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+        await collection.insertOne({
+            query,
+            type: "playlist",
+            uid,
+        });
+
+        res.send({
+            data: {
+                uid
+            },
+            status: 200
+        })
+    }catch(err){
+        console.log(err)
+        res.status(500).send({
+            data: err,
+            status: 500
+        })
     }
 }
