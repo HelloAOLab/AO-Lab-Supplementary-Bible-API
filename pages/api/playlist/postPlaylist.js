9dcfedc9b74334d8789da54a454698645d81cb26
Bored-Wizard <sujanbarman12345q@gmail.com>
2024-06-28 08:16:11 +0530

    test changes

--- pages/api/playlist/postPlaylist.js
+++ pages/api/playlist/postPlaylist.js
@@ -1,35 +1,39 @@
 import { connectToMongoDB, db } from "../../../lib/mongodb/mongodb";
 
 export default async function handler(req, res) {
-    try{
-        res.setHeader('Access-Control-Allow-Origin', 'https://ao.bot');
-        
-
-        const {query} = req.query;
-
-        if(!db){
-            await connectToMongoDB();
+    if (req.method === 'POST') {
+        try{
+            res.setHeader('Access-Control-Allow-Origin', 'https://ao.bot');
+            
+    
+            const {query} = req.body;
+    
+            if(!db){
+                await connectToMongoDB();
+            }
+    
+            const collection = db.collection('playlist');
+            const uid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+            await collection.insertOne({
+                query,
+                type: "playlist",
+                uid,
+            });
+    
+            res.send({
+                data: {
+                    uid
+                },
+                status: 200
+            })
+        }catch(err){
+            console.log(err)
+            res.status(500).send({
+                data: err,
+                status: 500
+            })
         }
-
-        const collection = db.collection('playlist');
-        const uid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-        await collection.insertOne({
-            query,
-            type: "playlist",
-            uid,
-        });
-
-        res.send({
-            data: {
-                uid
-            },
-            status: 200
-        })
-    }catch(err){
-        console.log(err)
-        res.status(500).send({
-            data: err,
-            status: 500
-        })
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
     }
 }
