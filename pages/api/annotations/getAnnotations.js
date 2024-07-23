a6bfc3200223018708f17e19165bfde646139251
Bored-Wizard <sujanbarman12345q@gmail.com>
2024-07-24 04:35:47 +0530

    updated annotations

--- pages/api/annotations/getAnnotations.js
+++ pages/api/annotations/getAnnotations.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
         const collection = db.collection('annotation');
 
-        if(!page || !count || (!query && !date)){
+        if(!page || !count){
             res.send({
                 data: "Invalid Request",
                 status: 400
@@ -25,21 +25,22 @@ export default async function handler(req, res) {
         // const annotations = await collection.find({title}).toArray();
 
         // find documents and sort them according to title field and title should not be null
-        if(query){
-            const annotations = await collection.find({title: {$ne: null},title: new RegExp(query, 'i')}).sort({title: 1}).skip((parseInt(page) - 1) * parseInt(count)).limit(parseInt(count)).toArray();
-            const nextAnnotations = await collection.find({title: {$ne: null},title: new RegExp(query, 'i')}).sort({title: 1}).skip((parseInt(page)) * parseInt(count)).limit(parseInt(count)).toArray();
-
-            res.send({
-                data: annotations,
-                nextCount: nextAnnotations.length,
-                status: 200
-            })
-        }else if(date){
+        if(date){
             console.log(new Date(date).toUTCString())
             const tomorrow = new Date(new Date(date).setDate(new Date(date).getDate() + 1)).toUTCString();
             console.log(tomorrow)
             const annotations = await collection.find({createdAt: {$gte: new Date(date).toUTCString(), $lte: tomorrow}}).sort({createdAt: -1}).skip((parseInt(page) - 1) * parseInt(count)).limit(parseInt(count)).toArray();
             const nextAnnotations = await collection.find({createdAt: {$gte: new Date(date).toUTCString(), $lte: tomorrow}}).sort({createdAt: -1}).skip((parseInt(page)) * parseInt(count)).limit(parseInt(count)).toArray();
+            res.send({
+                data: annotations,
+                nextCount: nextAnnotations.length,
+                status: 200
+            })
+        }else{
+            console.log(query)
+            const annotations = await collection.find({title: {$ne: null},title: new RegExp(query, 'i')}).sort({title: 1}).skip((parseInt(page) - 1) * parseInt(count)).limit(parseInt(count)).toArray();
+            const nextAnnotations = await collection.find({title: {$ne: null},title: new RegExp(query, 'i')}).sort({title: 1}).skip((parseInt(page)) * parseInt(count)).limit(parseInt(count)).toArray();
+
             res.send({
                 data: annotations,
                 nextCount: nextAnnotations.length,
