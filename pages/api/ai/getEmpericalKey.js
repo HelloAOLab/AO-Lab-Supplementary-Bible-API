a2bda7636f4ef949bbff17c5757772dff167dd35
Bored-Wizard <sujanbarman12345dq@gmail,com>
2025-09-24 22:43:46 +0530

    added api for geting emperical key for ai session

--- pages/api/ai/getEmpericalKey.js
+++ pages/api/ai/getEmpericalKey.js
@@ -1,6 +1,8 @@
 export default async function handler(req, res) {
     try{
-        res.setHeader('Access-Control-Allow-Origin', 'https://ao.bot');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
         const sessionConfig = JSON.stringify({
             session: {
